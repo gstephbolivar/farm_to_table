514bb55d062cb17fc580388f401facb06efb002d
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-02-02 13:50:22 -0500

    Set up mongoosedb in server file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -7,12 +8,32 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/farm-to-table",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected!");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+
 app.get("/api/config", (req, res) => {
-    res.json({
-        success: true,
-    })
-})
+  res.json({
+    success: true,
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
