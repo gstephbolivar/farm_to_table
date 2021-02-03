1a61f88c9abc2c8418f6389d2513fa028320c755
Robert Anderson <reanderson89@gmail.com>
2021-02-02 22:36:33 -0800

    added a cartItem and imported it into the cart.

---
+++ client/src/components/CartItem/CartItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartItem = () => {
+    return (
+        <div>
+            <h1> This is an item in the cart at checkout.</h1>
+        </div>
+    );
+};
+
+export default CartItem;
