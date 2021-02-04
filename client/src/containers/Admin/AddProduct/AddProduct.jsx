3451d7372cccb9fb09d12ae54bb2261325873303
Robert Anderson <reanderson89@gmail.com>
2021-02-03 18:32:27 -0800

    added card content to addPorduct container

--- client/src/containers/Admin/AddProduct/AddProduct.jsx
+++ client/src/containers/Admin/AddProduct/AddProduct.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import {Grid, Card} from '@material-ui/core';
+import {Grid, Card, CardMedia, CardContent} from '@material-ui/core';
 
 const AddProduct = () => {
-    return (
-        <Grid container>
 
+    const submit = () => {
+        alert("You have added a product.")
+    }
+
+    return (
+    <Grid container>
+        <Grid item>
+            <Card>
+                <CardMedia
+                component="img
+                height="auto
+                image="http://www.fillmurray.com/200/200"
+                title="Stock Image"/>
+                <CardContent>
+                    <Typography variant="h5"> Product Name Goes here </Typography>
+                    <Typography variant="body2"> Description goes here </Typography>
+                </CardContent>
+                <CardActionArea>
+                    <Button onClick={submit}>Add Product</Button>
+                </CardActionArea>
+            </Card>
         </Grid>
+    </Grid>
     );
 };
 
