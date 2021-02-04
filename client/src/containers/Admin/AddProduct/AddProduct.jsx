9613c0de2b7974e5ff9dc0c569b08bb615037112
Robert Anderson <reanderson89@gmail.com>
2021-02-03 19:31:14 -0800

    created form inputs. [WIP] select unit and set unit size.

--- client/src/containers/Admin/AddProduct/AddProduct.jsx
+++ client/src/containers/Admin/AddProduct/AddProduct.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, Card, CardMedia, CardContent} from '@material-ui/core';
+import {Grid, Card, CardMedia, CardContent, Typography, CardActionArea, Button, TextField, InputAdornment} from '@material-ui/core';
 
 const AddProduct = () => {
 
@@ -8,17 +8,58 @@ const AddProduct = () => {
     }
 
     return (
-    <Grid container>
-        <Grid item>
+    <Grid container direction="column"
+    alignItems="center"
+    justify="center">
+        <Grid item xs={6} xl={6}>
             <Card>
                 <CardMedia
-                component="img
-                height="auto
+                component="img"
+                height="auto"
                 image="http://www.fillmurray.com/200/200"
-                title="Stock Image"/>
+                title="Stock Image"
+                />
                 <CardContent>
-                    <Typography variant="h5"> Product Name Goes here </Typography>
-                    <Typography variant="body2"> Description goes here </Typography>
+                    <Grid container spacing={1}>
+                        <Grid item>
+                    {/* <Typography variant="h5"> Product Name Goes here </Typography>
+                    <Typography variant="body2"> Description goes here </Typography> */}
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Product Name"
+                        variant="outlined"
+                        />
+                        </Grid>
+                        <Grid item>
+                        <TextField
+                        id="outlined-multiline-static"
+                        label="Product Description"
+                        multiline
+                        rows={2}
+                        variant="outlined"
+                        />
+                        </Grid>
+                        <Grid item>
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Unit Size"
+                        variant="outlined"
+                        />
+                        </Grid>
+                        <Grid item>
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Price Per Unit"
+                        variant="outlined"
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                          }}
+                        />
+                        </Grid>
+                        </Grid>
                 </CardContent>
                 <CardActionArea>
                     <Button onClick={submit}>Add Product</Button>
