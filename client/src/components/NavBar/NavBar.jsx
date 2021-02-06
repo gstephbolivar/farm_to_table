d1d549e1994c864dbb7a08a448d9f2ada326ba61
Robert Anderson <reanderson89@gmail.com>
2021-02-05 17:55:01 -0800

    installed material ui icons. Got appbar up and running. [WIP] make dropdown menu with links to pages.

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {IconButton, Toolbar, Typography, Button, AppBar, Menu, MenuItem} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const NavBar = () => {
-    return (
-        <div>
-            <h1>This will be the navbar.</h1>
-        </div>
-    );
-};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-export default NavBar;
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
