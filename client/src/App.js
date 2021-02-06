e691feacd4fd560a94014cdde8a6bc868065922a
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2021-02-06 11:48:27 -0500

    Fixed admin routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home.jsx";
 import AllProducts from "./containers/AllProducts/AllProducts.jsx";
 import Cart from "./containers/Cart/Cart.jsx";
@@ -12,29 +12,26 @@ import EditProduct from "./containers/Admin/EditProduct/EditProduct";
 import CssBaseLine from "@material-ui/core/CssBaseline";
 import NavBar from "./components/NavBar/NavBar.jsx";
 
-
-
 function App() {
-  
   return (
     <>
-    <CssBaseLine/>
-    <BrowserRouter>
-    <NavBar/>
-      <Switch>
-        <Route path="/home" component={Home} />
-        <Route path="/allproducts" component={AllProducts} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/confirmation" component={Confirmation} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/oneproduct" component={OneProduct} />
-        <Route path="/admin" component={Products} />
-        <Route path="/admin/add" component={AddProduct} />
-        <Route path="/admin/edit" component={EditProduct} />
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
+      <CssBaseLine />
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/allproducts" component={AllProducts} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/confirmation" component={Confirmation} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/oneproduct" component={OneProduct} />
+          <Route exact path="/admin" component={Products} />
+          <Route exact path="/admin/add" component={AddProduct} />
+          <Route path="/admin/edit" component={EditProduct} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
