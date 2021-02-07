af3a3ece778eaadfe04be34a606a33095c3ff041
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-02-07 14:03:33 -0500

    Axios api route

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export default {
+
+        // product routes
+getAllProducts: () => {
+    return axios.get("/api/products")
+},
     
     // product routes
 addProduct: (product) => {
