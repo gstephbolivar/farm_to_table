1dfeb329c0527547819088934dfda92be3205bdd
Melanie Bostwick <mbostwick1@gmail.com>
2021-02-07 19:49:42 -0500

    home page placeholder done

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,15 +1,37 @@
 import React from "react";
-import {Grid, Typography} from '@material-ui/core';
-
-
+import { Grid, Typography } from "@material-ui/core";
 
 const Home = () => {
   return (
-    <Grid container alignItems="center" justify="center">
-      <Grid item xs={1} md={1} lg={1} xl={6} align="center">
-        <Typography variant="h1" align="center">Farm to Table</Typography>
-      </Grid>
-    </Grid>
+    <section class="section">
+      <div class="container has-text-centered">
+        <div class="columns is-centered is-multiline">
+          <div class="column is-8" id="column">
+            <img
+              src="https://www.placecage.com/g/500/300"
+              className="figure-img img-fluid rounded"
+              id="Logo"
+              alt=""
+            />
+            <h1 class="title">Farm to Table</h1>
+
+            <p id="about-me">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              nihil odit quisquam accusantium ipsum qui, fuga incidunt animi
+              ipsam molestiae rem, beatae amet! Tempore unde esse magnam nulla
+              necessitatibus nemo aspernatur voluptatem ratione ab itaque.
+              Doloremque sed veniam illo officia corrupti quam provident sequi
+              totam saepe, sit exercitationem pariatur quisquam.
+            </p>
+            <div class="buttons are-medium is-centered">
+              <button class="button" id="" href="" target="_blank">
+                Shop Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
