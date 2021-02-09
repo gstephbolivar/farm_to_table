458b4b51dfb7658d53ae1d94c5019d9fd41342c4
Melanie Bostwick <mbostwick1@gmail.com>
2021-02-08 20:47:45 -0500

    switched products to bulma, adjusted product card column

--- client/src/containers/AllProducts/AllProducts.jsx
+++ client/src/containers/AllProducts/AllProducts.jsx
@@ -1,21 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import CategoriesCard from "../../components/CategoriesCard/CategoriesCard";
-import { Grid } from "@material-ui/core";
 import axios from "axios";
 
-// CSS styling
-
-const useStyles = makeStyles({
-  productContainer: {
-    margin: "3rem auto",
-    marginLeft: "3rem auto",
-  },
-});
-
 const AllProducts = () => {
-  const classes = useStyles();
   const [products, setProducts] = useState([]);
 
   // load all cards and store them with setCard
@@ -31,23 +19,27 @@ const AllProducts = () => {
   }, []);
 
   return (
-    <>
-      <Grid container className={classes.productContainer}>
-        <Grid item xs>
-          <CategoriesCard />
-        </Grid>
-        {/* {card.map((products) => (
-          <Grid key={card._id }item xs>
-            <ProductCard />
-          </Grid>
-        ))} */}
-        {products.map((product) => (
-          <Grid item xs={3}>
-            <ProductCard {...product} key={product._id} />
-          </Grid>
-        ))}
-      </Grid>
-    </>
+    <div>
+      <section className="section">
+        <div className="container has-text-centered">
+          <div className="columns">
+            <div className="column is-2">
+              <div className="columns is-mobile">
+                <CategoriesCard />
+              </div>
+            </div>
+            <div className="column is-10">
+              <h3 className="title has-text-centered">Area for Search Bar</h3>
+              <div className="columns is-mobile is-centered is-multiline">
+                {products.map((product) => (
+                  <ProductCard {...product} key={product._id} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
