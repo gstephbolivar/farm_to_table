1a5029de1ff9d681e19612cd1fc0cd5841cc7ded
Rashawn Raiford <raiford87@gmail.com>
2021-02-11 00:16:57 -0500

    Added CartBadge component to NavBar component

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import {useContext} from "react";
+import CartContext from '../../utils/CartContext';
 import { makeStyles } from "@material-ui/core/styles";
 import { Toolbar, Typography, Button, AppBar } from "@material-ui/core";
-
+import CartBadge from '../CartBadge/CartBadge';
 import MenuDropdown from "../../components/MenuDropdown/MenuDropdown";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,6 +19,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const {lineItems} = useContext(CartContext);
+  const count = lineItems.reduce((total, current) => total + current.quantity, 0, lineItems, 0);
 
   return (
     <div className={classes.root}>
@@ -28,6 +31,7 @@ export default function ButtonAppBar() {
             News
           </Typography>
           <Button color="inherit">Login</Button>
+          <CartBadge count={count}/>
         </Toolbar>
       </AppBar>
     </div>
