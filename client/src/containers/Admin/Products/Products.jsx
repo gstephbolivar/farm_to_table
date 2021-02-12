bb3a33c404ef34c541925ae2d1ee4b9c644a68ef
Melanie Bostwick <mbostwick1@gmail.com>
2021-02-12 15:08:29 -0500

    commit before pull

--- client/src/containers/Admin/Products/Products.jsx
+++ client/src/containers/Admin/Products/Products.jsx
@@ -1,91 +1,90 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import ProductCard from "../../../components/ProductCard/ProductCard";
 import CategoriesCard from "../../../components/CategoriesCard/CategoriesCard";
 import API from "../../../utils/API";
 
 const Products = () => {
-    
-    const [products, setProducts] = useState([]);
-    const [categoryFilter, setCategoryFilter] = useState("");
+  const [products, setProducts] = useState([]);
+  const [categoryFilter, setCategoryFilter] = useState("");
 
-
-
-      // load all cards and store them with setCard
+  // load all cards and store them with setCard
   useEffect(() => {
     loadProducts();
   }, []);
 
-    const loadProducts = () => {
-        API.getProduct()
-          .then((response) => {
-            setProducts(response.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+  const loadProducts = () => {
+    API.getProduct()
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // sets the category filter to state
+  const filterProducts = (e) => {
+    const category = e.target.name;
+    if (category !== "All") {
+      // filters the products displayed based on the category
+      displayFilteredProducts(category);
+      console.log(category);
+      setCategoryFilter(category);
     }
+    // if All category is chosen, resets the filter
+    else loadProducts();
+  };
 
- // sets the category filter to state
- const filterProducts = (e) => {
-  const category = e.target.name;
-  if (category !== "All") {
-    // filters the products displayed based on the category
-    displayFilteredProducts(category);
-    console.log(category);
-    setCategoryFilter(category);
-  }
-  // if All category is chosen, resets the filter
-  else loadProducts();
-};
-
-const displayFilteredProducts = (category) => {
-  API.getAllProducts()
-    .then((response) => {
-      const filteredProducts = response.data.filter(
-        (p) => p.category.toLowerCase() === category.toLowerCase()
-      );
-      console.log(categoryFilter);
-      console.log(response.data);
-      console.log(filteredProducts);
-      setProducts(filteredProducts);
-    })
-    .catch((err) => console.log(err));
-};
-
-
-
+  const displayFilteredProducts = (category) => {
+    API.getAllProducts()
+      .then((response) => {
+        const filteredProducts = response.data.filter(
+          (p) => p.category.toLowerCase() === category.toLowerCase()
+        );
+        console.log(categoryFilter);
+        console.log(response.data);
+        console.log(filteredProducts);
+        setProducts(filteredProducts);
+      })
+      .catch((err) => console.log(err));
+  };
 
   const handleDeleteButton = (id) => {
     API.deleteProduct(id)
-    .then(res => loadProducts())
-    .catch(err => console.log(err))
+      .then((res) => loadProducts())
+      .catch((err) => console.log(err));
   };
 
-    useEffect(() => {
-        API.getProduct()
-          .then((response) => {
-            setProducts(response.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }, []);
+  useEffect(() => {
+    API.getProduct()
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-
-      <div>
+  return (
+    <div>
       <section className="section">
-      <h3 className="title has-text-centered">This will be where you can view all products. Admin side.</h3>
+        <h3 className="title has-text-centered">
+          This will be where you can view all products. Admin side.
+        </h3>
 
         <div className="container">
           <div className="columns">
             <div className="column">
-                <CategoriesCard onClick={filterProducts} />
+              <CategoriesCard onClick={filterProducts} />
             </div>
             <div className="column is-9">
               <div className="columns is-centered is-multiline">
                 {products.map((product) => (
-                  <ProductCard {...product} deleteProduct={handleDeleteButton} key={product._id}/>
+                  <ProductCard
+                    {...product}
+                    deleteProduct={handleDeleteButton}
+                    key={product._id}
+                  />
                 ))}
               </div>
             </div>
@@ -93,8 +92,7 @@ const displayFilteredProducts = (category) => {
         </div>
       </section>
     </div>
-
-    );
+  );
 };
 
 export default Products;
