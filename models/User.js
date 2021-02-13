19cf0e495b4f13366a46408e77fcb27e98e51ce9
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2021-02-13 12:13:21 -0500

    Changed backend to check for email address instead of username

--- models/User.js
+++ models/User.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
   // both first and last name - can create nested object if needed
   name: {
     type: String,
@@ -16,11 +21,6 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: ["Please enter a valid password!"],
-  },
-  email: {
-    type: String,
-    unique: true,
-    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
   },
 });
 
--- server.js
+++ server.js
@@ -197,7 +197,7 @@ app.post("/api/users", ({ body }, res) => {
 // GET api route to return selected user
 app.get("/api/users", (req, res) => {
   db.User.findOne({
-    username: req.query.username,
+    email: req.query.email,
     password: req.query.password,
   })
     .then((result) => {
