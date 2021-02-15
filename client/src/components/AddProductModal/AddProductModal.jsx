95064c2c7e3001d478e0d16d7a9c99cef8cfa215
Melanie Bostwick <mbostwick1@gmail.com>
2021-02-15 02:26:37 -0500

    add product modal styled

--- client/src/components/AddProductModal/AddProductModal.jsx
+++ client/src/components/AddProductModal/AddProductModal.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import API from "../../utils/API";
 import "./add.css";
 
-
 const productType = [
   {
     value: "fruit",
@@ -69,175 +68,177 @@ const AddProductModal = (props) => {
 
   return (
     <>
-    <div className="modal-background"></div>
-    <div className="modal-card">
-      <header className="modal-card-head">
-        <p className="modal-card-title">Add Product</p>
-      </header>
-      <section className="modal-card-body">
-    <section className="section">
-      <form className="create-form">
-        <div className="container has-text-centered">
-          <div className="column is-half is-offset-one-quarter">
-        
-            <img
-              title="Stock Image"
-              src="https://www.placecage.com/c/250/250"
-              alt="stock"
-              height="auto"
-            />
-            <br />
+      <div className="modal-background"></div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title has-text-centered add-product-headline">Add Product</p>
+        </header>
+        <section className="modal-card-body">
+            <form className="create-form">
+              <div className="container has-text-centered">
+                <div className="column is-10 is-offset-1">
+                  <img
+                    title="Stock Image"
+                    id="product-image"
+                    src="https://www.placecage.com/c/250/250"
+                    alt="stock"
+                    height="auto"
+                  />
+                  <br />
 
-            <div className="dropdown">
-              {/* What kind of product it is */}
+                  {/* What kind of product it is */}
 
-              <div className="field is-grouped is-grouped-centered">
+                  <div className="field is-grouped is-grouped-centered">
+                    <div className="field is-inline-block add-products-fields">
+                      <label className="label">Category</label>
+                      <div className="control">
+                        <div className="select">
+                          <select
+                            id="selectCategory"
+                            label="Select Type"
+                            name="category"
+                            onChange={handleInputChange}
+                            value={productObject.category}
+                          >
+                            {productType.map((option) => (
+                              <option key={option.value} value={option.value}>
+                                {option.label}
+                              </option>
+                            ))}
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                  
+                  {/* the name of the product */}
 
-              <div className="field is-inline-block">
-                <label className="label">Category</label>
-                <div className="control">
-                  <div className="select">
-                    <select
-                      id="selectCategory"
-                      label="Select Type"
-                      name="category"
-                      onChange={handleInputChange}
-                      value={productObject.category}
-                    >
-                      {productType.map((option) => (
-                        <option key={option.value} value={option.value}>
-                          {option.label}
-                        </option>
-                      ))}
-                    </select>
+                  <div className="field add-products-fields">
+                    <label className="label">Product</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Strawberries"
+                        id="productName"
+                        onChange={handleInputChange}
+                        name="name"
+                        value={productObject.name}
+                      />
+                    </div>
+                  </div>
+
+                  {/* This is the total number of "units" that are available to be sold. It is calculated for you as you enter the total amount of each product and the unit size to sell by. */}
+
+                  <div className="field add-products-fields">
+                    <label className="label">Quantity</label>
+                    <div className="control">
+                      <input
+                        required
+                        className="input"
+                        type="number"
+                        id="quantity"
+                        onChange={handleInputChange}
+                        name="quantity"
+                        value={productObject.quantity}
+                      />
+                    </div>
+                  </div>
+
+                  </div>
+
+                  <div className="field is-grouped is-grouped-centered">
+                  
+               {/* The size of which each unit will be sold (Example: you buy strawberries by the pound in most places, but costco sells them in 3 pound boxes. So a "unit" is either 1 pound or 3 pounds respectively.) */}
+
+                  <div className="field add-products-fields">
+                    <label className="label">Unit Size</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        id="unitSize"
+                        min="1"
+                        onChange={handleInputChange}
+                        name="unitSize"
+                        value={productObject.unitSize}
+                        required
+                        type="number"
+                      />
+                    </div>
+                  </div>
+
+                   {/* The unit type the product will be sold by */}
+                   <div className="field add-products-fields">
+                      <label className="label">Unit Type</label>
+                      <div className="control">
+                        <input
+                          className="input"
+                          id="unitType"
+                          required
+                          name="unitType"
+                          placeholder="lbs"
+                          value={productObject.unitType}
+                          onChange={handleInputChange}
+                          type="text"
+                        />
+                      </div>
+                    </div>
+
+                  {/* the price at which each unit is sold per unit */}
+                  <div className="field add-products-fields">
+                    <label className="label">Price</label>
+                    <div className="control has-icons-left">
+                      <span className="icon is-small is-left">$</span>
+                      <input
+                        className="input"
+                        id="productPrice"
+                        onChange={handleInputChange}
+                        name="price"
+                        value={productObject.price}
+                        data-type="currency"
+                        required
+                        type="number"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-              </div>
-              {/* the name of the product */}
-
-              <div className="field">
-                <label className="label">Product</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="Strawberries"
-                    id="productName"
-                    onChange={handleInputChange}
-                    name="name"
-                    value={productObject.name}
-                  />
                 </div>
-              </div>
-            
-              </div>
-            {/* description of the product */}
 
-            <div className="field">
-              <label className="label">Description</label>
-              <div className="control">
-                <textarea
-                  className="textarea"
-                  type="text"
-                  placeholder="Organic Strawberries"
-                  label="Product Description"
-                  multiline="true"
-                  rows={2}
-                  variant="outlined"
-                  onChange={handleInputChange}
-                  name="description"
-                  value={productObject.description}
-                ></textarea>
-              </div>
-            </div>
+                {/* description of the product */}
 
-            <div className="dropdown">
-              
-
-              {/* The unit type the product will be sold by */}
-              <div className="field is-inline-block">
-                <label className="label">Unit Type</label>
-                <div className="control">
-                  <input className="input"                 
-                      id="unitType"
-                      required
-                      name="unitType"
-                      value={productObject.unitType}
-                      onChange={handleInputChange}
+                <div className="field add-products-fields">
+                  <label className="label">Description</label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
                       type="text"
-                    />                  
+                      placeholder="Organic Strawberries"
+                      label="Product Description"
+                      multiline="true"
+                      rows={2}
+                      variant="outlined"
+                      onChange={handleInputChange}
+                      name="description"
+                      value={productObject.description}
+                    ></textarea>
                   </div>
                 </div>
               </div>
-            </div>
-
-            
-              {/* The size of which each unit will be sold (Example: you buy strawberries by the pound in most places, but costco sells them in 3 pound boxes. So a "unit" is either 1 pound or 3 pounds respectively.) */}
-
-              <div className="field">
-                <label className="label">Unit Size to Sell</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    id="unitSize"
-                    min="1"
-                    onChange={handleInputChange}
-                    name="unitSize"
-                    value={productObject.unitSize}
-                    required
-                    type="number"
-                  />
-                </div>
-              </div>
-
-              {/* the price at which each unit is sold per unit */}
-              <div className="field">
-                <label className="label">Price</label>
-                <div className="control has-icons-left">
-                  <span className="icon is-small is-left">$</span>
-                  <input
-                    className="input"
-                    id="productPrice"
-                    onChange={handleInputChange}
-                    name="price"
-                    value={productObject.price}
-                    data-type="currency"
-                    required
-                    type="number"
-                    step="0.01"
-                  />
-                </div>
-              </div>
-            
-
-            {/* This is the total number of "units" that are available to be sold. It is calculated for you as you enter the total amount of each product and the unit size to sell by. */}
-
-            <div className="field">
-            <label className="label">Quantity(Total Units)</label>
-              <div className="control">
-                <input
-                  required
-                  className="input"
-                  type="number"
-                  placeholder="Total units (inventory)"
-                  id="quantity"
-                  onChange={handleInputChange}
-                  name="quantity"
-                  value={productObject.quantity}
-                />
-              </div>
-            </div>
-        </div>
-      </form>
-    </section>
-    </section>
-    <footer className="modal-card-foot field is-grouped is-grouped-centered">
-      <button className="button" id="add-save"onClick={handleFormSubmit}>Save</button>
-      <button className="button" id="add-cancel" onClick={props.handleAddProductModalState}>Cancel</button>
-    </footer>
-  </div>
-</>
+            </form>
+        </section>
+        <footer className="modal-card-foot field is-grouped is-grouped-centered">
+          <button className="button" id="add-save" onClick={handleFormSubmit}>
+            Save
+          </button>
+          <button
+            className="button"
+            id="add-cancel"
+            onClick={props.handleAddProductModalState}
+          >
+            Cancel
+          </button>
+        </footer>
+      </div>
+    </>
   );
 };
 
