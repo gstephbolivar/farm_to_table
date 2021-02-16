28f8b214b23a550fafbb8e1a4074716deedeca5d
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-02-16 18:54:38 -0500

    Create contact card component

---
+++ client/src/components/ContactCard/ContactCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactCard = () => {
+    return (
+        <div>
+            <h1>this is our contact component</h1>
+        </div>
+    );
+};
+
+export default ContactCard;
