dd87afb5fc743e05f7584fb121c1c73a7c9db1c0
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-02-16 18:55:12 -0500

    Contact Card component renders on contact container

--- client/src/containers/Contact/Contact.jsx
+++ client/src/containers/Contact/Contact.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import ContactCard from "../../components/ContactCard/ContactCard"
 
 const Contact = () => {
-    return (
-        <div>
-            <h1>This is our contact page</h1>
+  return (
+    <>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column ">
+              <div className="columns is-centered is-multiline">
+                <h3 className="title has-text-centered products-headline">
+                  Contact us
+                </h3>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+
+        <div className="container">
+            <ContactCard />
+        </div>
+
+      </section>
+    </>
+  );
 };
 
 export default Contact;
