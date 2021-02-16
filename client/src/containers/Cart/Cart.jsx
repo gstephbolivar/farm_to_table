04df38609d6546ba5fee2df0ac113dfe674898c6
Rashawn Raiford <raiford87@gmail.com>
2021-02-16 13:57:13 -0500

    Added code to pass order id to call for email confirmation

--- client/src/containers/Cart/Cart.jsx
+++ client/src/containers/Cart/Cart.jsx
@@ -28,13 +28,14 @@ const Cart = (props) => {
       API.placeOrder({
         customer: userId,
         LineItem: res.data.map((x) => x._id),
-      }).then(() => {
+      }).then((order) => {
         localStorage.removeItem("lineItems");
         props.clearCart();
         API.getEmail(userId).then((result) => {
           API.sendConfirmationEmail({
             name: result.data.name,
             email: result.data.email,
+            orderId: order.data._id
           })
         })
       });
