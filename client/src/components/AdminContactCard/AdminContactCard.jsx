92e8d80256ada177770d534384099e91e2222f9c
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-02-16 19:13:17 -0500

    Created separate component contact cards depending on if a user or an admin is logged in

---
+++ client/src/components/AdminContactCard/AdminContactCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminContactCard = () => {
+    return (
+        <div className="card-content">
+            
+            <h1>AdminContactCard</h1>
+        </div>
+    );
+};
+
+export default AdminContactCard;
---
+++ client/src/components/UserContactCard/UserContactCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserContactCard = () => {
+    return (
+        <div>
+            <h1> UserContactCard</h1>
+        </div>
+    );
+};
+
+export default UserContactCard;
