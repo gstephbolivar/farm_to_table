517415457320291124e2db449944c9585eb9340b
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-02-16 19:20:48 -0500

    [WIP] Styling of contact card

--- client/src/components/UserContactCard/UserContactCard.jsx
+++ client/src/components/UserContactCard/UserContactCard.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const UserContactCard = () => {
-    return (
-        <div>
-            <h1> UserContactCard</h1>
-        </div>
-    );
+  return (
+    <div className="container has-text-centered">
+      <div className="columns is-centered is-multiline">
+        <h1>
+          {" "}
+          Thank you for supporting your community and shopping locally!
+          <br />
+          <br />
+          If you have any questions about your order please reach out to the
+          farm you placed your order from.
+          <br />
+          <br />
+          If you have any questions not related to your order please fill out
+          the contact form below and we'll respond as quickly as possible.
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default UserContactCard;
