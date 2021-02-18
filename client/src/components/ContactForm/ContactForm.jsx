eb333bf7c8132c121d035a37d8e43c5ef84ccec4
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-02-17 20:17:20 -0500

    Tried a different port and changed back to original port

--- client/src/components/ContactForm/ContactForm.jsx
+++ client/src/components/ContactForm/ContactForm.jsx
@@ -11,7 +11,7 @@ const ContactForm = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("http://localhost:3000/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
