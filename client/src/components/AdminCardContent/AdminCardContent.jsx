49afdfc39caac2b90390c6da90ff3c9fc388c3fc
Robert Anderson <reanderson89@gmail.com>
2021-02-19 14:44:28 -0800

    delete modal works, both the delete button and the out of season buttons work on the modal, and the X in the top right closes it with no changes.

--- client/src/components/AdminCardContent/AdminCardContent.jsx
+++ client/src/components/AdminCardContent/AdminCardContent.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import "./AdminCardContent.css";
+import API from "../../utils/API";
 
 const AdminCardContent = (props) => {
 
@@ -12,6 +13,17 @@ const AdminCardContent = (props) => {
       setDeleteModal("modal");
     }
   };
+
+  const handleMakeOutOfSeason = () => {
+    API.updateProduct(props._id, {category: "Out of Season"})
+    .then(() => {
+      handleDeleteModalState();
+      props.loadProducts();
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
 
     return (
       <>
@@ -50,22 +62,23 @@ const AdminCardContent = (props) => {
           <p className="modal-card-title has-text-centered add-product-headline">
             Delete Product
           </p>
+          <button className="delete" aria-label="close" onClick={handleDeleteModalState} ></button>
         </header>
         <section className="modal-card-body">
-          <p>Are you sure you want to delete this product?</p>
+          <p>Are you sure you want to delete this product from your inventory? If you think you will carry this product again please click "Out of Season" and it will no longer display to customers.</p>
         </section>
         <footer className="modal-card-foot field is-grouped is-grouped-centered">
           <button
-            className="button card-footer-item deleteButton"
+            className="button"
             onClick={props.handleDeleteButton}
           >
             Delete
           </button>
           <button
             className="button"
-            onClick={handleDeleteModalState}
+            onClick={handleMakeOutOfSeason}
           >
-            Cancel
+            Out of Season
           </button>
         </footer>
       </div>
