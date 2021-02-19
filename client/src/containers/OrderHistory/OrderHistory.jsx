9c87fff5db83e8955842f0fd251f2b1b390e05d3
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2021-02-19 13:28:01 -0500

    Added bulma message block for when there are no previous orders

--- client/src/containers/OrderHistory/OrderHistory.jsx
+++ client/src/containers/OrderHistory/OrderHistory.jsx
@@ -25,56 +25,68 @@ const OrderHistory = () => {
 
   return (
     <section className="container">
-      <div className="column">
-        {/* maps through the customerOrders state to create headings for each orderDate */}
-        {customerOrders.map((order) => (
-          <div className="column" key={order._id}>
-            <p className="panel-heading order-head">
-              Order Date {order.orderDate}
-            </p>
-            <table className="table is-fullwidth">
-              <thead>
-                <tr>
-                  <th>
-                    <div
-                      className="vertical-center"
-                      style={{
-                        height: 20,
-                        justifyContent: "left",
-                        padding: 10,
-                      }}
-                    >
-                      <h1 className="sub-title">Item</h1>
-                    </div>
-                  </th>
-                  <th className="is-vcentered">
-                    <div
-                      className="vertical-center"
-                      style={{ height: 20, justifyContent: "center" }}
-                    >
-                      <h1 className="sub-title">Quantity</h1>
-                    </div>
-                  </th>
-                  <th className="is-vcentered">
-                    <div
-                      className="vertical-center"
-                      style={{ height: 20, justifyContent: "center" }}
-                    >
-                      <h1 className="sub-title">Price</h1>
-                    </div>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* maps through the lineItems array in the customerOrders object to display each item in the specified order */}
-                {order.LineItem.map((line) => (
-                  <OrderHistoryItem lineItem={line} key={line._id} />
-                ))}
-              </tbody>
-            </table>
+      {/* if a customer has no previous orders, display message */}
+      {customerOrders.length === 0 ? (
+        <article className="message is-medium">
+          <div className="message-header">
+            <p>No Previous Orders</p>
           </div>
-        ))}
-      </div>
+          <div className="message-body">
+            Please place an order to start tracking.
+          </div>
+        </article>
+      ) : (
+        <div className="column">
+          {/* maps through the customerOrders state to create headings for each orderDate */}
+          {customerOrders.map((order) => (
+            <div className="column" key={order._id}>
+              <p className="panel-heading order-head">
+                Order Date {order.orderDate}
+              </p>
+              <table className="table is-fullwidth">
+                <thead>
+                  <tr>
+                    <th>
+                      <div
+                        className="vertical-center"
+                        style={{
+                          height: 20,
+                          justifyContent: "left",
+                          padding: 10,
+                        }}
+                      >
+                        <h1 className="sub-title">Item</h1>
+                      </div>
+                    </th>
+                    <th className="is-vcentered">
+                      <div
+                        className="vertical-center"
+                        style={{ height: 20, justifyContent: "center" }}
+                      >
+                        <h1 className="sub-title">Quantity</h1>
+                      </div>
+                    </th>
+                    <th className="is-vcentered">
+                      <div
+                        className="vertical-center"
+                        style={{ height: 20, justifyContent: "center" }}
+                      >
+                        <h1 className="sub-title">Price</h1>
+                      </div>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* maps through the lineItems array in the customerOrders object to display each item in the specified order */}
+                  {order.LineItem.map((line) => (
+                    <OrderHistoryItem lineItem={line} key={line._id} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };
